e71889fbc4590de4d642a70b9e67956a8e0a4f8d
UemKaLaunda <dasanishka157@gmail.com>
2026-03-19 14:36:48 +0530

    Modified the Application Starter code

--- src/main/java/com/capgemini/training/UemSpringBootMvcApplication.java
+++ src/main/java/com/capgemini/training/UemSpringBootMvcApplication.java
@@ -6,7 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UemSpringBootMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UemSpringBootMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        // Get PORT from environment (Railway)
+        String port = System.getenv("PORT");
+
+        // If not present (local), use 8080
+        if (port == null || port.isEmpty()) {
+            port = "8080";
+        }
+
+        // Set server port dynamically
+        System.setProperty("server.port", port);
+
+        SpringApplication.run(UemSpringBootMvcApplication.class, args);
+    }
 }
